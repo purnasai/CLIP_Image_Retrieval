f5f2ca01aa289232c61d9363fa122d057c75cf34
purnasai <purnasaigudikandula.com>
2023-05-23 18:15:15 +0530

    factorized code

--- image_retrieval/Generate_features.py
+++ image_retrieval/Generate_features.py
@@ -4,8 +4,9 @@ Description:This file generates image features from
         Database of images & stores them h5py file.
 """
 import os
-import h5py
 import random
+
+import h5py
 import numpy as np
 from tqdm import tqdm
 from PIL import Image
@@ -16,38 +17,54 @@ from torchvision import transforms
 
 from transformers import CLIPModel, CLIPProcessor
 
-model_name = "openai/clip-vit-base-patch32" 
-model = CLIPModel.from_pretrained(model_name)
-processor = CLIPProcessor.from_pretrained(model_name)
+MODEL_NAME = "openai/clip-vit-base-patch32"
+model = CLIPModel.from_pretrained(MODEL_NAME)
+processor = CLIPProcessor.from_pretrained(MODEL_NAME)
 
 def get_labels(files):
+    """
+    This function takes a list of file paths and returns a list of unique labels extracted from the
+    directory names in the file paths.
+    
+    :param files: a list of file paths (strings) that include the directory and filename, separated
+    by backslashes ("\") on Windows or forward slashes ("/") on Unix-based systems
+    :return: a list of unique labels extracted from the file paths provided in the `files` parameter.
+    """
     labels = []
     for file_path in files:
-        directory, filename = file_path.split("\\")
+        directory, _ = file_path.split("\\")
         directory_parts = directory.split("/")
         label = directory_parts[-1]
         if label not in labels:
             labels.append(label)
     return labels
+
+def list_files(dataset_path):
+    """
+    This function returns a list of all files in a directory and its subdirectories.
     
-def list_files(dir):
+    :param dir: The directory path where you want to list all the files
+    :return: The function `list_files` returns a list of file paths for all the files in the directory
+    and its subdirectories.
+    """
     images = []
-    for root, dirs, files in os.walk(dir):
+    for root, _, files in os.walk(dataset_path):
         for name in files:
             images.append(os.path.join(root, name))
     return images
 
 
 class CustomImageDataset(Dataset):
+    """The above class is a custom dataset class for images in PyTorch."""
     def __init__(self, img_dir):
         self.img_dir = img_dir
-        self.images = list_files(self.img_dir)[:1000]
+        self.images = list_files(self.img_dir)
         random.choices(self.images, k=5)
         self.transform =  transforms.Compose([
                             transforms.Resize(256),
                             transforms.CenterCrop(224),
                             transforms.ToTensor(),
-                            transforms.Normalize([0.485, 0.456, 0.406], 
+                            transforms.Normalize([0.485, 0.456, 0.406],
                                                 [0.229, 0.224, 0.225])
                         ])
 
@@ -60,7 +77,7 @@ class CustomImageDataset(Dataset):
         if self.transform:
             image = self.transform(image)
         return image, img_path
-    
+
 
 dir_path = "./Data/"
 dataset = CustomImageDataset(dir_path)
@@ -83,6 +100,6 @@ for image_tensors, file_paths in tqdm(train_dataloader):
 
 with h5py.File('features/Jewellery_features.h5','w') as h5f:
     h5f.create_dataset("jewellery_features", data= np.array(final_img_features))
-    # to save file names strings in byte format. 
+    # to save file names strings in byte format.
     h5f.create_dataset("jewellery_filenames", data= np.array(final_img_filepaths,
                                                              dtype=object))
---
+++ image_retrieval/image_search.py
@@ -0,0 +1,59 @@
+"""
+Author:Purnasai
+Description:This file loads h5py file and 
+           searches for match image.
+"""
+import random
+import h5py
+import faiss
+
+from PIL import Image
+import numpy as np
+
+from torchvision import transforms
+from transformers import CLIPModel, CLIPProcessor
+
+MODEL_NAME = "openai/clip-vit-base-patch32"
+model = CLIPModel.from_pretrained(MODEL_NAME)
+processor = CLIPProcessor.from_pretrained(MODEL_NAME)
+
+transform =  transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(224),
+                transforms.ToTensor(),
+                transforms.Normalize([0.485, 0.456, 0.406],
+                                    [0.229, 0.224, 0.225])
+            ])
+
+# Open the HDF5 file for reading
+with h5py.File('features/Jewellery_features.h5', 'r') as h5f:
+    # Read the dataset named "jewellery_features"
+    jewellery_features = np.array(h5f['jewellery_features'])
+    # Read the dataset named "jewellery_names"
+    jewellery_filenames = np.array(h5f['jewellery_filenames'])
+
+
+# Print the shape of the arrays to verify the data
+print("jewellery_features shape:", type(jewellery_features), jewellery_features.shape)
+print("jewellery_names shape:", jewellery_filenames.shape, type(jewellery_filenames))
+print("sample:", random.choices(jewellery_filenames,k =5))
+
+# The Inner Product similarity is often used in scenarios 
+# where vectors represent semantic or conceptual features.
+faiss_index = faiss.IndexFlatIP(jewellery_features.shape[1])
+faiss_index.add(jewellery_features)
+
+# L2norm is Euclidean distance, i.e disimilarity. 100-disimarlity
+# faiss_index =  faiss.IndexFlatL2(jewellery_features.shape[1])
+# faiss_index.add(jewellery_features)
+
+image = Image.open("./Data/Rings\\C023064_32.jpeg")
+t_image = transform(image).unsqueeze(dim=0)
+querry_features = model.get_image_features(t_image)
+querry_features /= querry_features.norm(dim=-1, keepdim=True)
+querry_features = querry_features.detach().numpy()
+
+K_neighbours = 10  # number of neighbors to retrieve
+distances, indices = faiss_index.search(querry_features, K_neighbours)
+for index in range(K_neighbours):
+    print(jewellery_filenames[indices[0][index]], distances[0][index]*100)
